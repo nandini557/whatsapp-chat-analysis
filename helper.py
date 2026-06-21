faee951fbc9296b085d88950f91c3b041e94f3eb
Nandini Agarwal <nandiniagarwal@Nandinis-Laptop.Dlink>
2026-06-22 00:47:23 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -24,6 +24,12 @@ def fetch_stats(selected_user,df):
     for message in df['message']:
         links.extend(extract.find_urls(message))
 
+def most_busy_users(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+    return x,df
+
 
     return num_messages,len(words),num_media_messages,len(links)
 
