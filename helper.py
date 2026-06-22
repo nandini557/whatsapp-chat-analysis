4c7e0f0e7788d8b1e2bb4cfaca37530cc50be20b
Nandini Agarwal <nandiniagarwal@Nandinis-Laptop.Dlink>
2026-06-22 16:01:53 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -37,14 +37,19 @@ def most_busy_users(df):
 
     return x, percent_df
 
-def create_wordcloud(selected_user,df):
+def create_wordcloud(selected_user, df):
 
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
 
-    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
-    df_wc=wc.generate(df['message'].str.cat(sep=" "))
-    return df_wc
+    print("Selected User:", selected_user)
+    print("Messages:", df.shape[0])
+
+    wc = WordCloud(width=500, height=500,
+                   min_font_size=10,
+                   background_color='white')
+
+    return wc.generate(df['message'].str.cat(sep=" "))
 
 
     
