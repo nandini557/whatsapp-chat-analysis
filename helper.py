0a3962b6be42427533282877019e7b1422a8514a
Nandini Agarwal <nandiniagarwal@Nandinis-Laptop.Dlink>
2026-06-22 16:06:59 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -37,24 +37,28 @@ def most_busy_users(df):
 
     return x, percent_df
 
-def create_wordcloud(selected_user, df):
+def create_wordcloud(selected_user,df):
+
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
 
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
 
-    df = df[df['user'] != 'group_notification']
-    df = df[df['message'] != '<Media omitted>\n']
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
 
-    text = df['message'].str.cat(sep=" ")
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
 
-    wc = WordCloud(
-        width=500,
-        height=500,
-        min_font_size=10,
-        background_color='white'
-    )
-
-    return wc.generate(text)
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    temp['message'] = temp['message'].apply(remove_stop_words)
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    return df_wc
 
 
     
