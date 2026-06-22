988539530e67a465e654c94ce6af34094984639e
Nandini Agarwal <nandiniagarwal@Nandinis-Laptop.Dlink>
2026-06-22 16:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,11 +55,11 @@ if uploaded_file is not None:
                 st.dataframe(new_df)
 
         #wordcloud
-            st.title("WordCloud")
-            df_wc=helper.create_wordcloud(selected_user,df)
-            fig,ax = plt.subplots()
-            ax.imshow(df_wc)
-            st.pyplot(fig)
+        st.title("WordCloud")
+        df_wc=helper.create_wordcloud(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
             
 
 
