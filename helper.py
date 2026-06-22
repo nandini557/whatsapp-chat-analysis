0c6b6fc4c5346e3dd662610e99193f04c5e7ade9
Nandini Agarwal <nandiniagarwal@Nandinis-Laptop.Dlink>
2026-06-22 15:09:53 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,37 +1,37 @@
-
 from urlextract import URLExtract
+
 extract = URLExtract()
-print("HELPER FILE LOADED - VERSION 4")
-def fetch_stats(selected_user,df):
-    print("RETURNING 4 VALUES")
+
+def fetch_stats(selected_user, df):
 
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
 
-    # fetch the number of messages
+    # Number of messages
     num_messages = df.shape[0]
 
-    # fetch the total number of words
+    # Number of words
     words = []
     for message in df['message']:
         words.extend(message.split())
 
-    
-     # fetch number of media messages
+    # Media messages
     num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
 
-    links =[]
+    # Links
+    links = []
     for message in df['message']:
         links.extend(extract.find_urls(message))
 
-    return num_messages,len(words),num_media_messages,len(links)
+    return num_messages, len(words), num_media_messages, len(links)
+
 
 def most_busy_users(df):
     x = df['user'].value_counts().head()
-    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
-        columns={'index': 'name', 'user': 'percent'})
-    return x,df
 
+    percent_df = (
+        round((df['user'].value_counts() / df.shape[0]) * 100, 2)
+        .reset_index()
+    )
 
-    
-
+    return x, percent_df
