f2fc0aa666d4b16dfd20e9ef2532afb0fdf44440
Nandini Agarwal <nandiniagarwal@Nandinis-Laptop.Dlink>
2026-06-22 16:32:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,18 @@ if uploaded_file is not None:
         fig,ax = plt.subplots()
         ax.imshow(df_wc)
         st.pyplot(fig)
+
+        # most common words
+        most_common_df = helper.most_common_words(selected_user,df)
+
+        fig,ax = plt.subplots()
+
+        ax.barh(most_common_df[0],most_common_df[1])
+        plt.xticks(rotation='vertical')
+
+        st.title('Most commmon words')
+        st.pyplot(fig)
+
             
 
 
